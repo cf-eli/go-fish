456c1cc2f95e1879284ee66018a259c53ca89bd0
AH135 <57057240+AH135@users.noreply.github.com>
2021-10-31 15:51:32 -0500

    updated player
    Used arraylist as a hand

--- src/gofish/Player.java
+++ src/gofish/Player.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package gofish;
+import java.util.ArrayList;
 
 /**
  *
@@ -11,20 +12,30 @@ package gofish;
  */
 public class Player {
     private String playername;
-    private Card[] hand;
+    private ArrayList<Card> hand;
     private int score;
     
     public Player(String name){
         playername = name;
     }
     public int getCard(int a){
-    return a;
+        return a;
     }
-    public void getName(){}
-    public void addHand(){}
-    public void removeHand(){}
-    public void getScore(){}
-    public void increaseScore(){}
+    public String getName(){
+        return this.playername;
+    }
+    public void addHand(Card c){
+        this.hand.add(c);
+    }
+    public void removeHand(Card c){
+        this.hand.remove(c);
+    }
+    public int getScore(){
+        return this.score;
+    }
+    public void increaseScore(){
+        this.score ++;
+    }
     
     
 }
