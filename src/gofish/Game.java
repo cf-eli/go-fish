3254c69643c898da514560c3e7b81d422a43e56b
AH135 <57057240+AH135@users.noreply.github.com>
2021-10-30 22:29:06 -0500

    added game class
    barebones game class added

---
+++ src/gofish/Game.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gofish;
+
+/**
+ *
+ * 
+ */
+public class Game {
+    String playerturn;
+    Boolean isgameover;
+    public Game(){//constructor for the game class, might need player ids or something
+    
+    }
+    public void startGame(){//method to start the game
+    
+    }
+    public void endGame(){}
+    public void checkGameOver(){}
+    public void setGameOver(){}
+    public void getPlayerTurn(){}
+    public void setPlayerTurn(){}
+    public void addPlayer(){}
+    
+    
+    
+}
+
+
