756e92998ba6ca9d2de2ba145dedec2b9fba3b0f
AH135 <57057240+AH135@users.noreply.github.com>
2021-11-03 15:55:14 -0500

    updated game functions
    added game over check and some more private variables for the game class

--- src/gofish/Game.java
+++ src/gofish/Game.java
@@ -4,26 +4,49 @@
  * and open the template in the editor.
  */
 package gofish;
+import java.util.*;
 
 /**
  *
  * 
  */
 public class Game {
-    private String playerturn;
+    private String currentplayerturn;
+    private String lastplayerturn;
     private Boolean isgameover;
-    public Game(){//constructor for the game class, might need player ids or something
+    private ArrayList<Player> players = new ArrayList<Player>();
     
+    
+    
+    
+    public Game(Player a, Ai b){//constructor for the game class, might need player ids or something
+        players.add(a);
+        players.add(b);
+        isgameover = false;
+        //need to designate who the current turn is here, either through a method or just choosing the first person in the list.
+        
+        
+        
     }
     public void startGame(){//method to start the game
     
     }
     public void endGame(){}
-    public void checkGameOver(){}
-    public void setGameOver(){}
-    public void getPlayerTurn(){}
+    
+    public void checkGameOver(){
+        for(int i =0; i<=players.size(); i++){
+            if (players.get(i).getScore()>=8){
+                isgameover = true;
+                //this.gameOver();//not functioning yet, not sure if it should go here yet
+            }
+        }
+            
+    }
+    public String getPlayerTurn(){
+        return this.currentplayerturn;
+    }
+    
     public void setPlayerTurn(){}
-    public void addPlayer(){}
     
     
     
