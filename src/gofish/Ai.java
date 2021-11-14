84ea437b0cf6a00bdbd779bc3a63dd20488811a1
AH135 <57057240+AH135@users.noreply.github.com>
2021-11-14 09:15:10 -0600

    fixed up a bit of the backend
    added what i had saved locally over the last few weeks

--- src/gofish/Ai.java
+++ src/gofish/Ai.java
@@ -10,12 +10,16 @@ package gofish;
  * @author Vuilk
  */
 public class Ai extends Player{
+    private int difficulty;//set this to 0-2, 0= easy, 1 = normal, 2 = hard
     
     Ai(){
         super("computer");
+        //this just needs functions specific to the Ai player. so the function decideing what card to pick can go here. need to figure out how to handle difficulty. maybe a memory bank of the last cards you have or something
     
     
     }
+    public void setDifficulty(){}
+    
     
     
     
--- src/gofish/Game.java
+++ src/gofish/Game.java
@@ -13,6 +13,7 @@ import java.util.*;
 public class Game {
     private String currentplayerturn;
     private String lastplayerturn;
+    private String nextplayerturn;
     private Boolean isgameover;
     private ArrayList<Player> players = new ArrayList<Player>();
     
@@ -34,7 +35,7 @@ public class Game {
     public void endGame(){}
     
     public void checkGameOver(){
-        for(int i =0; i<=players.size(); i++){
+        for(int i=0; i<=players.size(); i++){
             if (players.get(i).getScore()>=8){
                 isgameover = true;
                 //this.gameOver();//not functioning yet, not sure if it should go here yet
@@ -46,7 +47,11 @@ public class Game {
         return this.currentplayerturn;
     }
     
-    public void setPlayerTurn(){}
+    public void setPlayerTurn(){
+        //need this to check the last player before setting the next person in the arrylist as the currentplayer turn. should always check to be sure its not doubleing up a turn or something.
+        //this might be involed. we need to decide if we want to just move the arraylist of players around, and designateing the first slot as the "current" turn, or storeing the turns seporately in variables or something, not sure which is better. 
+        
+    }
     
     
     
