2ddc583255bb796e63a2f20f980c47b4640055ef
Alex Hannen <ac-hannen@wiu.edu>
2022-04-23 02:28:42 -0500

    added card tests

--- src/test/java/edu/wiu/gofish/CardTest.java
+++ src/test/java/edu/wiu/gofish/CardTest.java
@@ -6,14 +6,54 @@ package edu.wiu.gofish;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith; 
+import org.junit.runners.Parameterized; 
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  *
  * @author Alex
  */
+//expected result, result
+@RunWith(Parameterized.class)
 public class CardTest {
+    private int inputrank;
+    private int inputsuit;
+    private String expectedStringRank;
+    private String expectedStringSuit;
+    private int expectedintRank;
+    private int expectedintSuit;
+            
     
-    public CardTest() {
+    public CardTest(int inputrank, int inputsuit, String expectedrank, String expectedsuit, int expectedintRank, int expectedintSuit) {
+        this.inputrank = inputrank;
+        this.inputsuit = inputsuit;
+        this.expectedStringRank = expectedrank;
+        this.expectedStringSuit = expectedsuit;
+        this.expectedintRank = expectedintRank;
+        this.expectedintSuit = expectedintSuit;
+        
+        
+    }
+    
+    @Parameters
+    public static Collection<Object[]> data() {
+        Collection<Object[]> data = new ArrayList<>();      
+        
+        for(int i = 0; i < 4; i++){ //4 suits
+            for(int j = 1; j < 14; j++) //13 cards per suit
+            {
+                //CardTest.decktest.add(new Card(j,i));
+                Object[] test = new Object[]{j,i,Card.RANK[j],Card.SUIT[i], j, i};
+                data.add(test);
+                
+            }     
+        }
+        return data;
     }
 
     /**
@@ -22,12 +62,10 @@ public class CardTest {
     @Test
     public void testGetRank() {
         System.out.println("getRank");
-        Card instance = null;
-        int expResult = 0;
-        int result = instance.getRank();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Card cardtest = new Card(inputrank,inputsuit);
+        
+        assertEquals("test",expectedintRank,cardtest.getRank());
+        System.out.println(expectedintRank+" == "+cardtest.getRankString());
     }
 
     /**
@@ -36,12 +74,9 @@ public class CardTest {
     @Test
     public void testGetSuit() {
         System.out.println("getSuit");
-        Card instance = null;
-        int expResult = 0;
-        int result = instance.getSuit();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Card cardtest = new Card(inputrank,inputsuit);
+        
+        assertEquals(expectedintSuit,cardtest.getSuit());
     }
 
     /**
@@ -49,13 +84,10 @@ public class CardTest {
      */
     @Test
     public void testGetSuitString() {
-        System.out.println("getSuitString");
-        Card instance = null;
-        String expResult = "";
-        String result = instance.getSuitString();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        Card cardtest = new Card(inputrank,inputsuit);
+        
+        assertEquals(expectedStringSuit,cardtest.getSuitString());
     }
 
     /**
@@ -63,13 +95,9 @@ public class CardTest {
      */
     @Test
     public void testGetRankString() {
-        System.out.println("getRankString");
-        Card instance = null;
-        String expResult = "";
-        String result = instance.getRankString();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Card cardtest = new Card(inputrank,inputsuit);
+        
+        assertEquals(expectedStringRank,cardtest.getRankString());
     }
 
     /**
